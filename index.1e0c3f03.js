1ce6890d5af8804861d36c1c12fe1b77065bdbcd
KtianG <92620631+KtianG@users.noreply.github.com>
2023-03-16 20:20:05 +0000

    Deploying to gh-pages from @ KtianG/canvas-sandbox@7e0b40597d869927631630356a87bdbf1adbdcc4 🚀

---
+++ index.1e0c3f03.js
@@ -0,0 +1,2 @@
+!function(){var n=document.querySelector("#canvas1");ctx=n.getContext("2d"),n.width=window.innerWidth,n.height=window.innerHeight;var e={x:null,y:null};window.addEventListener("mousemove",(function(n){e.x=n.x,e.y=n.y}))}();
+//# sourceMappingURL=index.1e0c3f03.js.map
---
+++ index.6f73781c.js
@@ -0,0 +1,2 @@
+const n=document.querySelector("#canvas1");ctx=n.getContext("2d"),n.width=window.innerWidth,n.height=window.innerHeight;const e={x:null,y:null};window.addEventListener("mousemove",(n=>{e.x=n.x,e.y=n.y}));
+//# sourceMappingURL=index.6f73781c.js.map
