f4b3d7e9a584da59671a6a52609d4896b3124ac9
KtianG <92620631+KtianG@users.noreply.github.com>
2023-03-16 21:08:48 +0000

    Deploying to gh-pages from @ KtianG/canvas-sandbox@38086873276618b703b196dcbb357a5c1abb5ff1 🚀

--- index.6f73781c.js
+++
@@ -1,2 +0,0 @@
-const n=document.querySelector("#canvas1");ctx=n.getContext("2d"),n.width=window.innerWidth,n.height=window.innerHeight;const e={x:null,y:null};window.addEventListener("mousemove",(n=>{e.x=n.x,e.y=n.y}));
-//# sourceMappingURL=index.6f73781c.js.map
